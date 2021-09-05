38f72079bbf2d2a3a44190ef88a6d219bf1463fc
BurhanuddinMerchant1 <ilinkhackathon3@gmail.com>
2021-09-05 15:15:26 +0530

    added the checkout page 🏁

---
+++ checkout/checkout.js
@@ -0,0 +1,37 @@
+function runSpeechRecognition() {
+  var Name = document.getElementById("name");
+  Name.innerHTML = "Name: " + sessionStorage.getItem("Name");
+  var Score = document.getElementById("score");
+  Score.innerHTML = "Score: " + sessionStorage.getItem("Score");
+  // new speech recognition object
+  var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+  var recognition = new SpeechRecognition();
+  recognition.continuous = true;
+  // This runs when the speech recognition service starts
+  recognition.onstart = function () {};
+
+  // This runs when the speech recognition service returns result
+  recognition.onresult = function (event) {
+    var transcript = event.results[event.results.length - 1][0].transcript;
+    console.log("inside result", event.results);
+    if (transcript.toLowerCase().includes("back")) {
+      window.location.pathname = "./game/";
+    } else if (transcript.toLowerCase().includes("home")) {
+      window.location.pathname = "./";
+    } else if (transcript.toLowerCase().includes("read")) {
+      speak(
+        "Flappy Thanks, You have scored:" +
+          sessionStorage.getItem("Score") +
+          ". Say: Back, to play the game else say: home to navigate to the home page!"
+      );
+    }
+  };
+
+  // start recognition
+  recognition.start();
+}
+function speak(text) {
+  var msg = new SpeechSynthesisUtterance("No warning should arise");
+  msg.text = text;
+  window.speechSynthesis.speak(msg);
+}
